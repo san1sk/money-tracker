6f81beb2e3f407b3cc219087b693771cbf8316a6
Sanchit Khokhar <88581704+san1sk@users.noreply.github.com>
2024-02-18 15:15:55 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
-const express = require("express");
 // import express from 'express';
+// 9rz20t1ILI2O4nxh
+const express = require("express");
+const cors = require('cors')
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const { default: mongoose } = require("mongoose");
 const app = express();
 
-app.get('/api/test',(req,res)=>{
+app.use(cors());
+app.use(express.json());
+app.get('/api/test', (req, res) => {
     res.json('test ok');
+});
+
+app.post('/api/transaction', async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const { name, description, datetime, price } = req.body;
+    const transaction = await Transaction.create({
+        name, description, datetime, price
+    });
+    res.json(req.body);
 });
 
 app.listen(4000);
--- src/App.js
+++ src/App.js
@@ -8,63 +8,80 @@ const App = () => {
   const [description, setDescription] = useState('');
   function addNewTransaction(ev) {
     ev.preventDefault();
-    const url = process.env.REACT_APP_API_URL;
-    console.log(url);
-  }
-  return (
-    <main>
-      <h1>$400<span>.00</span></h1>
-      <form onSubmit={addNewTransaction}>
-        <div className='basic'>
-          <input type="text"
-            value={name}
-            onChange={ev => setName(ev.target.value)}
-            placeholder={'+100 new mobile'} />
-          <input value={datetime}
-            onChange={ev => setDatetime(ev.target.value)}
-            type="datetime-local" />
+    const url = process.env.REACT_APP_API_URL + '/transaction';
+    const price = name.split(' ')[0];
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        price,
+        name: name.substring(price.length + 1),
+      description,
+      datetime,
+    })
+  }).then(response => {
+    response.json().then(json => {
+      setName('');
+      setDatetime('');
+      setDescription('');
+      console.log('result', json);
+    });
+  });
+}
+return (
+  <main>
+    <h1>$400<span>.00</span></h1>
+    <form onSubmit={addNewTransaction}>
+      <div className='basic'>
+        <input type="text"
+          value={name}
+          onChange={ev => setName(ev.target.value)}
+          placeholder={'+100 new mobile'} />
+        <input value={datetime}
+          onChange={ev => setDatetime(ev.target.value)}
+          type="datetime-local" />
+      </div>
+      <div className='description'>
+        <input value={description}
+          onChange={ev => setDescription(ev.target.value)}
+          type="text" placeholder={'description'} />
+      </div>
+      <button type='submit'>Add new transaction</button>
+    </form>
+    <div className="transactions">
+      <div className="transaction">
+        <div className="left">
+          <div className="name">New mobile</div>
+          <div className="description">It was time for new mobile phone</div>
         </div>
-        <div className='description'>
-          <input value={description}
-            onChange={ev => setDescription(ev.target.value)}
-            type="text" placeholder={'description'} />
-        </div>
-        <button type='submit'>Add new transaction</button>
-      </form>
-      <div className="transactions">
-        <div className="transaction">
-          <div className="left">
-            <div className="name">New mobile</div>
-            <div className="description">It was time for new mobile phone</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$500</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Salary</div>
-            <div className="description">It was time for new mobile phone</div>
-          </div>
-          <div className="right">
-            <div className="price green">+$400</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
-        </div>
-        <div className="transaction">
-          <div className="left">
-            <div className="name">Laptop</div>
-            <div className="description">It was time for new mobile phone</div>
-          </div>
-          <div className="right">
-            <div className="price red">-$600</div>
-            <div className="datetime">2023-12-18 15:45</div>
-          </div>
+        <div className="right">
+          <div className="price red">-$500</div>
+          <div className="datetime">2023-12-18 15:45</div>
         </div>
       </div>
-    </main>
-  )
+      <div className="transaction">
+        <div className="left">
+          <div className="name">Salary</div>
+          <div className="description">It was time for new mobile phone</div>
+        </div>
+        <div className="right">
+          <div className="price green">+$400</div>
+          <div className="datetime">2023-12-18 15:45</div>
+        </div>
+      </div>
+      <div className="transaction">
+        <div className="left">
+          <div className="name">Laptop</div>
+          <div className="description">It was time for new mobile phone</div>
+        </div>
+        <div className="right">
+          <div className="price red">-$600</div>
+          <div className="datetime">2023-12-18 15:45</div>
+        </div>
+      </div>
+    </div>
+  </main>
+)
 }
 
 export default App
